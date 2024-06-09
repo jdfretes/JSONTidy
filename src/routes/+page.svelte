d1fc0312b1d79a2729582d00642f5c07f5f035a5
Juan Fretes <jdfretes@roshka.com>
2024-06-09 19:43:18 -0400

    ok

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>JSON Formatter</h1>
+<textarea id="input" placeholder="Paste your JSON here..."></textarea>
+<button>Format JSON</button>
+<textarea id="output" placeholder="Formatted JSON will appear here..."></textarea>
